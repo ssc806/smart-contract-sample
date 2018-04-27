55be0ea82a0a753a51d179525e7551052ca584f3
Shuchao Shao <ssc806@gmail.com>
2018-04-26 20:56:20 -0500

    Update the contract address

--- parse-smart-contract.js
+++ parse-smart-contract.js
@@ -26,8 +26,7 @@ const fromAddress = '0xcAb6aD272D18A52ECd8fc1F37398AbA675145990'
 //const tokenHolderAddress = '0xf0ee1855d7a54cf7436ad5866e02f34d0b835ba5';
 
 //WANCHAIN - Testnet
-//0x3150894F182dc46555586C88C2d42c70ed4B32c6
-const contractAddress = '0x3150894F182dc46555586C88C2d42c70ed4B32c6';
+const contractAddress = '0xa4b45e5d5eee0eb8305ee6c7b621979aac31df69';
 const tokenHolderAddress = '0xcAb6aD272D18A52ECd8fc1F37398AbA675145990';
 
 const tokenContract = web3.eth.contract(abi).at(contractAddress);
@@ -55,7 +54,7 @@ tokenContract.totalSupply.call (function(err, totalSupply) {
 	if(err) { 
 		console.log(err);
 	} else {
-		console.log('Token total supply: ' + totalSupply);
+		console.log('Token total supply: ' + totalSupply.div(1e18));
 	}
 })
 
@@ -69,8 +68,8 @@ tokenContract.balanceOf.call (tokenHolderAddress, function(err, balance){
 })
 
 //Transfer tokens
-web3.personal.unlockAccount(fromAddress, '******', 60)
-tokenContract.transfer('0xc333d44b565bebf4045b662a36353a841f96d98f', 10*1e18, {from: fromAddress}, function (err, txHash) {
+web3.personal.unlockAccount(fromAddress, 'viral123', 60)
+tokenContract.transfer('0xc333d44b565bebf4045b662a36353a841f96d98f', 1*1e18, {from: fromAddress}, function (err, txHash) {
 	if (err) {
 	    console.error(err)
 	} else {
@@ -79,7 +78,7 @@ tokenContract.transfer('0xc333d44b565bebf4045b662a36353a841f96d98f', 10*1e18, {f
 })
 
 //Parse input data
-const txHash = '0x2449a564e8beed4eb3eae23e870f827c67eaaf87f509a03686c2d24f0bca775d'
+const txHash = '0x5802e819eb97a56a3fb0e77c58be30b1675f64caff0e508ca50045d9744e0617'
 web3.eth.getTransaction(txHash, function(err, result) { 
 	if (err) {
 		console.log(err);
@@ -90,7 +89,7 @@ web3.eth.getTransaction(txHash, function(err, result) {
 		abiDecoder.addABI(abi);
 		const decodedData = abiDecoder.decodeMethod(inputData);
 
-		//console.log(decodedData);
+		console.log(decodedData);
 
 		console.log('From Address: ' + result.from);
 		console.log('To Address: ' + decodedData.params[0].value);
